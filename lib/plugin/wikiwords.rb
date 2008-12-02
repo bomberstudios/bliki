645f1b7f65610c785a5bc0effcd8246d6e35348d
Ale Muñoz <bomberstudios@gmail.com>
2008-12-02 19:59:10 +0100

    Strip white space on WikiWord links

--- lib/plugin/wikiwords.rb
+++ lib/plugin/wikiwords.rb
@@ -1,7 +1,7 @@
 module BlikiContent
   def plugin_wikiwords content
     content.gsub(/\s([A-Z]+)([a-z]+)([A-Z]+)\w+\s/) do |m|
-      " <a href=\"#{Sinatra.options.base_url}/#{m.downcase}\">#{m.strip}</a> "
+      " <a href=\"#{Sinatra.options.base_url}/#{m.strip.downcase}\">#{m.strip}</a> "
     end
   end
 end
