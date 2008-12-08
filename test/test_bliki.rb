7493ec4324c415bcf99b9bcd33e187d91afc2636
Ale Muñoz <bomberstudios@gmail.com>
2008-12-08 11:01:35 +0100

    Require redgreen for tests

--- test/test_bliki.rb
+++ test/test_bliki.rb
@@ -7,17 +7,18 @@ require 'bliki'
 require 'feed_validator'
 require 'fileutils'
 require 'feed_validator/assertions'
+require 'redgreen'
 
 class Test::Unit::TestCase
   def self.test(name, &block)
-    test_name = "test_#{name.gsub(/\s+/,'_')}".to_sym
+    test_name = "test_#{name.gsub(/\s+/,'_')}".downcase.to_sym
     defined = instance_method(test_name) rescue false
     raise "#{test_name} is already defined in #{self}" if defined
     if block_given?
       define_method(test_name, &block)
     else
       define_method(test_name) do
-        flunk "No implementation provided for #{name}"
+        flunk "No implementation provided for '#{name}'"
       end
     end
   end
@@ -29,6 +30,7 @@ class BlikiTest < Test::Unit::TestCase
     Dir["public/**/*"].each do |file|
       FileUtils.rm file
     end
+    FileUtils.mkdir_p "test/public"
     Dir["test/public/**/*"].each do |file|
       FileUtils.rm file
     end
